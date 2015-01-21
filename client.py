ba33bdd791c0dadaa1a366f1b02cd8bd981d6c68
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2015-01-21 14:41:14 +0100

    New client CLI param --expected-length

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ Exit codes:
 2   - Received strange response / not a Reflector server?
 
 128 - The reflector server was checking the address but HTTP status code != 200
+129 - The reflector server was checking the address but the expected length didn't match.
 
 """
 
@@ -25,6 +26,7 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('reflector_address', help='The address of your reflector server.')
     parser.add_argument('check_address', help='The address of the server you want to check.')
+    parser.add_argument('--expected-length', '-l', type=int, help='Specify a Enable verbose debug mode.')
     parser.add_argument('--debug', action="store_true", help='Enable verbose debug mode.')
     args = parser.parse_args()
 
@@ -37,14 +39,22 @@ def main():
         if 'status' not in response:
             if args.debug: print("A problem at the server side occured / not a http_reflector server?")
             sys.exit(2)
-        if response['status'] == 'success':
-            if args.debug: print("The server is reachable!")
-            sys.exit(0)
         if response['status'] == 'error':
             if args.debug: print("The server is not reachable!")
             sys.exit(128)
+        if response['status'] == 'success':
+            if args.expected_length == None:
+                if args.debug: print("The server is reachable!")
+                sys.exit(0)
+            else:
+                if args.expected_length == response['length']:
+                    if args.debug: print("The server is reachable and the length fits.")
+                    sys.exit(0)
+                else:
+                    if args.debug: print("The server was reachable but the length didn't fit!")
+                    sys.exit(129)
     except urllib.error.URLError as e:
-        if args.debug: print("Could not reach the reflector server '{}'. Error: '{}'.".format(args.reflector_address, str(e)))
+        if args.debug: print("Could not reach the reflector server '{}'! Error: '{}'.".format(args.reflector_address, str(e)))
         sys.exit(1)
 
 if __name__ == '__main__':
