21193c39448d3209ad0c4e10938ca8235c003495
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2015-01-21 11:58:09 +0100

    client: added exit codes / new --debug CLI flag

--- client.py
+++ client.py
@@ -2,6 +2,16 @@
 
 """
 Bottle-based HTTP Reflector Client
+
+Exit codes:
+
+0   - Server reachable, returned HTTP status code 200
+
+1   - Reflector server not reachable
+2   - Received strange response / not a Reflector server?
+
+128 - The reflector server was checking the address but HTTP status code != 200
+
 """
 
 import sys
@@ -15,16 +25,26 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('reflector_address', help='The address of your reflector server.')
     parser.add_argument('check_address', help='The address of the server you want to check.')
+    parser.add_argument('--debug', action="store_true", help='Enable verbose debug mode.')
     args = parser.parse_args()
 
     check_address = urllib.parse.quote(args.check_address)
     checkurl = urllib.parse.urljoin(args.reflector_address, '/check/' + check_address)
-    print("HTTP request to {}".format(checkurl))
+    if args.debug: print("HTTP request to {}".format(checkurl))
     try:
         response = json.loads(urllib.request.urlopen(checkurl).read().decode('utf-8'))
-        print(response)
+        if args.debug: print(response)
+        if 'status' not in response:
+            if args.debug: print("A problem at the server side occured / not a http_reflector server?")
+            sys.exit(2)
+        if response['status'] == 'success':
+            if args.debug: print("The server is reachable!")
+            sys.exit(0)
+        if response['status'] == 'error':
+            if args.debug: print("The server is not reachable!")
+            sys.exit(128)
     except urllib.error.URLError as e:
-        print("Could not reach the reflector server '{}'. Error: '{}'.".format(args.reflector_address, str(e)))
+        if args.debug: print("Could not reach the reflector server '{}'. Error: '{}'.".format(args.reflector_address, str(e)))
         sys.exit(1)
 
 if __name__ == '__main__':
